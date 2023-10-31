b6f2f33fcf8459857023ed70289721c2687a22c6
dawwson <shines1427@gmail.com>
2023-10-31 21:08:12 +0900

    add: 글로벌 유효성 검사 파이프 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get<string>('SERVER_PORT');
