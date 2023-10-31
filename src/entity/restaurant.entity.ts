32855fc00b1b8d929c118f1baf1962e5a69d9583
dawwson <shines1427@gmail.com>
2023-10-31 21:05:48 +0900

    comment: 주석 위치 수정

--- src/entity/restaurant.entity.ts
+++ src/entity/restaurant.entity.ts
@@ -33,65 +33,65 @@ export class Restaurant {
   @Column({ type: 'varchar', length: 100 }) // 시군명
   cityName!: string;
 
-  @Column() // 위도
-  latitude!: number;
+  @Column()
+  latitude!: number; // 위도
 
-  @Column() // 경도
-  longitude!: number;
+  @Column()
+  longitude!: number; // 경도
 
-  @Column({ default: 0 }) // 조회수
-  viewCount!: number;
+  @Column({ default: 0 })
+  viewCount!: number; // 조회수
 
-  @Column({ default: 0.0 }) // 총평점
-  totalRating!: number;
+  @Column({ type: 'double', default: 0.0 })
+  totalRating!: number; // 총평점
 
-  @Column({ nullable: true }) // 인허가일자
-  licenseDate?: Date;
+  @Column({ nullable: true })
+  licenseDate?: Date; // 인허가일자
 
-  @Column({ nullable: true }) // 폐업일자
-  closeDate?: Date;
+  @Column({ nullable: true })
+  closeDate?: Date; // 폐업일자
 
-  @Column({ nullable: true, type: 'varchar', length: 50 }) // 소재지면
-  locationArea?: string;
+  @Column({ nullable: true, type: 'varchar', length: 50 })
+  locationArea?: string; // 소재지면
 
-  @Column({ nullable: true, type: 'varchar', length: 255 }) // 급수시설구분명
-  waterworksName?: string;
+  @Column({ nullable: true, type: 'varchar', length: 255 })
+  waterworksName?: string; // 급수시설구분명
 
-  @Column({ nullable: true }) // 남성종사자수
-  maleEmployeeCount?: number;
+  @Column({ nullable: true })
+  maleEmployeeCount?: number; // 남성종사자수
 
-  @Column({ nullable: true }) // 여성종사자수
-  femaleEmployeeCount?: number;
+  @Column({ nullable: true })
+  femaleEmployeeCount?: number; // 여성종사자수
 
-  @Column({ nullable: true, type: 'varchar', length: 20 }) // 연도
-  year?: string;
+  @Column({ nullable: true, type: 'varchar', length: 20 })
+  year?: string; // 연도
 
-  @Column({ nullable: true }) // 다중이용업소여부
-  isMultiUse?: boolean;
+  @Column({ nullable: true })
+  isMultiUse?: boolean; // 다중이용업소여부
 
-  @Column({ nullable: true, type: 'varchar', length: 100 }) // 등급구분명
-  gradeName?: string;
+  @Column({ nullable: true, type: 'varchar', length: 100 })
+  gradeName?: string; // 등급구분명
 
-  @Column({ nullable: true, type: 'varchar', length: 100 }) // 총시설규모
-  totalFacilityScale?: string;
+  @Column({ nullable: true, type: 'varchar', length: 100 })
+  totalFacilityScale?: string; // 총시설규모
 
-  @Column({ nullable: true, type: 'varchar', length: 100 }) // 영업장주변구분명
-  surroundingAreaName?: string;
+  @Column({ nullable: true, type: 'varchar', length: 100 })
+  surroundingAreaName?: string; // 영업장주변구분명
 
-  @Column({ nullable: true, type: 'varchar', length: 100 }) // 위생업종명
-  industryType?: string;
+  @Column({ nullable: true, type: 'varchar', length: 100 })
+  industryType?: string; // 위생업종명
 
-  @Column({ nullable: true }) // 총종업원수
-  totalEmployeeCount?: string;
+  @Column({ nullable: true })
+  totalEmployeeCount?: string; // 총종업원수
 
-  @Column({ nullable: true, type: 'varchar', length: 200 }) // 소재지지번주소
-  lotNumberAddress?: string;
+  @Column({ nullable: true, type: 'varchar', length: 200 })
+  lotNumberAddress?: string; // 소재지지번주소
 
-  @Column({ nullable: true, type: 'varchar', length: 30 }) // 우편번호
-  zipCode?: string;
+  @Column({ nullable: true, type: 'varchar', length: 30 })
+  zipCode?: string; // 우편번호
 
-  @Column({ nullable: true }) // 시군코드
-  cityCode?: number;
+  @Column({ nullable: true })
+  cityCode?: number; // 시군코드
 
   @CreateDateColumn()
   createdAt!: Date;
