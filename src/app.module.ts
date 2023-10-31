0973e23cf8142b68e2c0142824abf1487b631f8a
dawwson <shines1427@gmail.com>
2023-10-31 20:20:53 +0900

    add: config 모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './feature/auth/auth.module';
 import { UserModule } from './feature/user/user.module';
 import { RestaurantModule } from './feature/restaurant/restaurant.module';
 import { CityModule } from './feature/city/city.module';
 
 @Module({
-  imports: [AuthModule, UserModule, RestaurantModule, CityModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    AuthModule,
+    UserModule,
+    RestaurantModule,
+    CityModule,
+  ],
   providers: [],
 })
 export class AppModule {}
