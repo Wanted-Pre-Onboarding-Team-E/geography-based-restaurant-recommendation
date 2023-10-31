5060aafdf5dc9b7e4e0ec736b4ad55c6727d6a9e
dawwson <shines1427@gmail.com>
2023-10-31 20:03:18 +0900

    add: restaurant 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './feature/auth/auth.module';
 import { UserModule } from './feature/user/user.module';
+import { RestaurantModule } from './feature/restaurant/restaurant.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, RestaurantModule],
   providers: [],
 })
 export class AppModule {}
