42549c4803ad6caa4e5bced170bc8e79ae933e97
Lee seongwon <jsw6357@naver.com>
2023-11-03 15:11:30 +0900

    feat:시군구 목록 고도화API 생성

--- src/feature/restaurant/restaurant.module.ts
+++ src/feature/restaurant/restaurant.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { RestaurantController } from './restaurant.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/entity/restaurant.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Restaurant])],
   controllers: [RestaurantController],
   providers: [RestaurantService],
 })
