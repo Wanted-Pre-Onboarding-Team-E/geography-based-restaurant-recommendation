1d5e630a6e7b0dc2d7e90b57f2d5fa82c6b8d2af
Lee seongwon <jsw6357@naver.com>
2023-11-03 15:11:30 +0900

    feat:시군구 목록 고도화 함수 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-
 import { User } from './entity/user.entity';
 import { Restaurant } from './entity/restaurant.entity';
 import { Review } from './entity/review.entity';
-
 import { AuthModule } from './feature/auth/auth.module';
 import { UserModule } from './feature/user/user.module';
 import { RestaurantModule } from './feature/restaurant/restaurant.module';
 import { CityModule } from './feature/city/city.module';
-
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -39,6 +38,13 @@ import { CityModule } from './feature/city/city.module';
     UserModule,
     RestaurantModule,
     CityModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 100000,
+    }),
   ],
   providers: [],
 })
--- src/feature/restaurant/restaurant.service.ts
+++ src/feature/restaurant/restaurant.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as csv from 'csv-parser';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
-export class RestaurantService {}
+export class RestaurantService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  //NOTE: 서버 실행 시 한번, 이후 하루에 한번 코드 실행
+  @Cron('0 0 * * * *')
+  async cacheCSV() {
+    const filePath = path.join(__dirname, '../../../csv', 'sgg_lat_lon.csv');
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        const ds = row['ds'];
+        const sgg = row['sgg'];
+        const lon = parseFloat(row['lon']);
+        const lat = parseFloat(row['lat']);
+
+        this.cacheManager.set(`${ds}_${sgg}`, { lon, lat });
+      })
+      .on('end', () => {
+        console.log('데이터를 캐시에 저장했습니다.');
+      });
+  }
+}
