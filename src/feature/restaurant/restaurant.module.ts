ae9b22212806c4190ddf66afaf9e81cff085ffbf
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-06 20:49:29 +0900

    add: 맛집 목록 조회 기능 추가

--- src/feature/restaurant/restaurant.module.ts
+++ src/feature/restaurant/restaurant.module.ts
@@ -4,9 +4,10 @@ import { RestaurantController } from './restaurant.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from '../../entity/restaurant.entity';
 import { RestaurantLib } from './restaurant.lib';
+import { UtilModule } from '../../util/util.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant])],
+  imports: [TypeOrmModule.forFeature([Restaurant]), UtilModule],
   controllers: [RestaurantController],
   providers: [RestaurantService, RestaurantLib],
   exports: [RestaurantLib, RestaurantService],
--- src/feature/restaurant/restaurant.service.ts
+++ src/feature/restaurant/restaurant.service.ts
@@ -1,14 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Restaurant } from '../../entity/restaurant.entity';
+import { UtilService } from '../../util/util.service';
+import { FailType } from '../../enum/failType.enum';
 
 @Injectable()
 export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant)
     private readonly restaurantRepository: Repository<Restaurant>,
+    private readonly utilService: UtilService,
   ) {}
+
+  async getRestaurants(
+    lat: number,
+    lon: number,
+    range: number,
+    sortBy: 'distance' | 'total_rating',
+    orderBy: 'ASC' | 'DESC',
+    search: string = '',
+    pageCount: number,
+    page: number,
+  ) {
+    if (!lat || !lon) {
+      throw new BadRequestException(FailType.RESTAURANT_LOCATION_REQUIRE);
+    }
+
+    return await this.restaurantRepository
+      .createQueryBuilder('r')
+      .select(`r.id`, `id`)
+      .addSelect(`r.placeName`, `placeName`)
+      .addSelect(`r.business_type`, `business_type`)
+      .addSelect(`r.business_state`, `business_state`)
+      .addSelect(`r.road_name_address`, `road_name_address`)
+      .addSelect(`r.city_name`, `city_name`)
+      .addSelect(`r.latitude`, `latitude`)
+      .addSelect(`r.longitude`, `longitude`)
+      .addSelect(`r.view_count`, `view_count`)
+      .addSelect(`r.total_rating`, `total_rating`)
+      .where(`r.placeName LIKE :placeName`, {
+        placeName: `%${search}%`,
+      })
+      .limit(pageCount)
+      .offset((page - 1) * pageCount)
+      .getRawMany()
+      .then((result: any[]) => {
+        return result
+          .filter((item) => {
+            return (
+              this.utilService.latLonToKm(
+                [item.latitude, item.longitude],
+                [lat, lon],
+              ) <
+              range * 1000
+            );
+          })
+          .map((item) => {
+            return {
+              ...item,
+              distance: this.utilService.latLonToKm(
+                [item.latitude, item.longitude],
+                [lat, lon],
+              ),
+            };
+          })
+          .sort((a, b) =>
+            orderBy === 'DESC'
+              ? b[`${sortBy}`] - a[`${sortBy}`]
+              : a[`${sortBy}`] - b[`${sortBy}`],
+          );
+      });
+  }
 
   async getRestaurantDetailById(id: number): Promise<Restaurant> {
     const restaurant = await this.restaurantRepository
@@ -43,9 +106,9 @@ export class RestaurantService {
       .createQueryBuilder()
       .update(Restaurant)
       .set({ viewCount: () => 'viewCount + 1' })
-      .where('id = :id', { id })
+      .where('id = :id', { id });
   }
-  
+
   async findOneBy(restaurantId: number): Promise<Restaurant> {
     return await this.restaurantRepository.findOneBy({ id: restaurantId });
   }
