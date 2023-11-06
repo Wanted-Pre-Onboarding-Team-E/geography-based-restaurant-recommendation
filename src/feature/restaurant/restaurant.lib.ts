3ca863bb8db3a64237185033c199360cbd1fe53c
dawwson <shines1427@gmail.com>
2023-11-06 22:50:23 +0900

    comment: 함수 설명 주석 추가

--- src/feature/restaurant/restaurant.lib.ts
+++ src/feature/restaurant/restaurant.lib.ts
@@ -12,8 +12,9 @@ export class RestaurantLib {
 
   /**
    * 총 평점이 높은 순으로 3.0/5.0 이상의 맛집 리스트를 불러온다.
+   * @return Restaurant 객체 배열
    */
-  getHighTotalRatingRestaurants() {
+  getHighTotalRatingRestaurants(): Promise<Restaurant[]> {
     return this.restaurantRepository.find({
       where: { totalRating: MoreThanOrEqual(3.0) },
       order: { totalRating: 'DESC' },
