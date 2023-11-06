30f979402357400e181e2104eb68888525330aad
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-06 20:49:29 +0900

    add: 맛집 목록 조회 기능 추가

--- src/feature/restaurant/restaurant.controller.ts
+++ src/feature/restaurant/restaurant.controller.ts
@@ -1,16 +1,51 @@
 import {
   Controller,
   Get,
-  Param,
   NotFoundException,
+  Param,
+  Query,
+  Res,
 } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { SuccessType } from '../../enum/successType.enum';
 import { FailType } from '../../enum/failType.enum';
+import { Response } from 'express';
 
 @Controller('restaurants')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
+
+  /**
+   * */
+  @Get('/')
+  async getRestaurants(
+    @Query('lat') lat: number,
+    @Query('lon') lon: number,
+    @Query('range') range: number,
+    @Query('sortBy') sortBy: 'distance' | 'total_rating' = 'distance',
+    @Query('orderBy') orderBy: 'ASC' | 'DESC' = 'ASC',
+    @Query('search') search: string = '',
+    @Query('pageCount') pageCount: number = 10,
+    @Query('page') page: number = 1,
+    @Res() res: Response,
+  ) {
+    console.log(lat, lon, range, sortBy, orderBy, search, pageCount, page);
+    const restaurants = await this.restaurantService.getRestaurants(
+      lat,
+      lon,
+      range,
+      sortBy,
+      orderBy,
+      search,
+      pageCount,
+      page,
+    );
+
+    return res.send({
+      message: SuccessType.RESTAURANT_LIST_GET,
+      data: restaurants,
+    });
+  }
 
   @Get('/:id')
   async getRestaurantAndAddViewCountById(@Param('id') id: number) {
