e57de5cd7c5ef24a5d17147840a34cf8ac6e8aea
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-06 20:48:22 +0900

    add: 시군구 목록 조회 기능 추가

--- src/feature/city/city.controller.ts
+++ src/feature/city/city.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { CityService } from './city.service';
+import { SuccessType } from '../../enum/successType.enum';
+import { Response } from 'express';
 
 @Controller('cities')
 export class CityController {
   constructor(private readonly cityService: CityService) {}
-  // FIXME: 엔드포인트 연결 여부를 확인하기 위해 임시로 작성했습니다. 함수명은 원하시는대로 수정해주세요~!
+
   @Get('/')
-  getCities() {
-    return 'GET /cities';
+  async getCities(@Res() res: Response) {
+    return res.send({
+      message: SuccessType.CITY_FETCH_DISTRICTS,
+      data: await this.cityService.selectCities(),
+    });
   }
 }
--- src/feature/city/city.module.ts
+++ src/feature/city/city.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CityService } from './city.service';
 import { CityController } from './city.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 100000,
+    }),
+  ],
   controllers: [CityController],
   providers: [CityService],
 })
