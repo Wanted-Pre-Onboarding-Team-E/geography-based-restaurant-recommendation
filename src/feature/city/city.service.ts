5619f5d85f42e699c624449496836c60b4fca982
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-06 20:48:22 +0900

    add: 시군구 목록 조회 기능 추가

--- src/feature/city/city.service.ts
+++ src/feature/city/city.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cron } from '@nestjs/schedule';
+import { Cache } from 'cache-manager';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as csv from 'csv-parser';
 
 @Injectable()
-export class CityService {}
+export class CityService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {
+    this.cacheCSV().then(() => {
+      console.log('init');
+    });
+  }
+
+  async selectCities() {
+    const cachedData = await this.cacheManager.get(`city`);
+
+    if (cachedData) {
+      return cachedData;
+    } else {
+      return this.cacheCSV();
+    }
+  }
+
+  @Cron('0 0 * * * *')
+  async cacheCSV() {
+    const filePath = path.join(__dirname, '../../../csv', 'sgg_lat_lon.csv');
+
+    const data = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (row) => {
+        console.log(row);
+        data.push({
+          city: row['\u200Bdo-si'],
+          district: row['sgg'],
+          latitude: row['lat'],
+          longitude: row['lon'],
+        });
+      })
+      .on('end', () => {
+        this.cacheManager.set(`city`, data);
+        return this.cacheManager.get(`city`);
+      });
+  }
+}
