0fba9f381f5542ce0b8012907b639aeeb04e3a7b
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-07 23:12:42 +0900

    refactor: 시군구 조회 단위 테스트 수정

--- test/unit/service/city.service.spec.ts
+++ test/unit/service/city.service.spec.ts
@@ -1,18 +1,49 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
 import { CityService } from '../../../src/feature/city/city.service';
+import { FailType } from '../../../src/enum/failType.enum';
 
 describe('CityService', () => {
-  let service: CityService;
+  let cityService: CityService;
+  let cacheManager: any; // Mocked cacheManager
+  let externalApiLib: any;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CityService],
-    }).compile();
+  beforeEach(() => {
+    cacheManager = {
+      get: jest.fn(),
+      set: jest.fn(),
+    };
+    externalApiLib = {
+      updateCities: jest.fn(),
+    };
 
-    service = module.get<CityService>(CityService);
+    cityService = new CityService(cacheManager, externalApiLib);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(cityService).toBeDefined();
+  });
+
+  describe('getCities', () => {
+    it('캐시 데이터로 시군구 조회', async () => {
+      const cachedData = ['City1', 'City2', 'City3'];
+      cacheManager.get.mockResolvedValue(cachedData);
+
+      const result = await cityService.getCities();
+
+      expect(result).toEqual(cachedData);
+      expect(cacheManager.get).toHaveBeenCalledWith('city');
+    });
+
+    it('캐시 데이터가 없고, 시군구 csv 파일까지 문제가 있을 경우', async () => {
+      cacheManager.get.mockResolvedValue(null);
+      externalApiLib.updateCities.mockResolvedValue(null);
+
+      try {
+        await cityService.getCities();
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toBe(FailType.CITY_NOT_FOUND);
+      }
+    });
   });
 });
