f516ee5477142bc2cc78130e73ff6bbf9d6ac405
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-07 23:12:49 +0900

    refactor: 맛집 조회 단위 테스트 수정

--- test/unit/controller/restaurant.controller.spec.ts
+++ test/unit/controller/restaurant.controller.spec.ts
@@ -1,20 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RestaurantController } from '../../../src/feature/restaurant/restaurant.controller';
 import { RestaurantService } from '../../../src/feature/restaurant/restaurant.service';
+import { GetRestaurantsDto } from '../../../src/feature/restaurant/dto/getRestaurant.dto';
+import { SuccessType } from '../../../src/enum/successType.enum';
 
 describe('RestaurantController', () => {
-  let controller: RestaurantController;
+  let restaurantController: RestaurantController;
+  let mockRestaurantService = {
+    getRestaurants: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RestaurantController],
-      providers: [RestaurantService],
+      providers: [
+        {
+          provide: RestaurantService,
+          useValue: mockRestaurantService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<RestaurantController>(RestaurantController);
+    restaurantController =
+      module.get<RestaurantController>(RestaurantController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(restaurantController).toBeDefined();
+  });
+
+  describe('getRestaurants()', () => {
+    it('맛집 조회', async () => {
+      const sampleRestaurants = ['Restaurant1', 'Restaurant2', 'Restaurant3'];
+      jest
+        .spyOn(mockRestaurantService, 'getRestaurants')
+        .mockResolvedValue(sampleRestaurants);
+
+      const lat = 123.45;
+      const lon = 67.89;
+      const getRestaurantsDto: GetRestaurantsDto = {
+        range: 1000,
+        sortBy: 'distance',
+        orderBy: 'ASC',
+        search: 'restaurant',
+        pageCount: 10,
+        page: 1,
+      };
+
+      const result = await restaurantController.getRestaurants(
+        lat,
+        lon,
+        getRestaurantsDto,
+      );
+
+      expect(result).toEqual({
+        message: SuccessType.RESTAURANT_LIST_GET,
+        data: sampleRestaurants,
+      });
+
+      expect(mockRestaurantService.getRestaurants).toHaveBeenCalledWith(
+        lat,
+        lon,
+        getRestaurantsDto,
+      );
+    });
   });
 });
--- test/unit/service/restaurant.service.spec.ts
+++ test/unit/service/restaurant.service.spec.ts
@@ -1,18 +1,106 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Cache } from 'cache-manager';
 import { RestaurantService } from '../../../src/feature/restaurant/restaurant.service';
+import { GetRestaurantsDto } from '../../../src/feature/restaurant/dto/getRestaurant.dto';
+import { FailType } from '../../../src/enum/failType.enum';
+import { UtilService } from '../../../src/util/util.service';
+import { Restaurant } from '../../../src/entity/restaurant.entity';
 
 describe('RestaurantService', () => {
-  let service: RestaurantService;
+  let restaurantService: RestaurantService;
+  let cacheManager: Cache;
+
+  const mockRestaurantService = {
+    getRestaurants: jest.fn(),
+  };
+
+  const mockRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RestaurantService],
+      providers: [
+        RestaurantService,
+        {
+          provide: getRepositoryToken(Restaurant), // Inject a mock repository
+          useValue: mockRepository,
+        },
+        UtilService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: cacheManager,
+        },
+      ],
     }).compile();
 
-    service = module.get<RestaurantService>(RestaurantService);
+    restaurantService = module.get<RestaurantService>(RestaurantService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(restaurantService).toBeDefined();
+  });
+
+  describe('getRestaurants', () => {
+    it('1km 이내 음식점 1개 조회', async () => {
+      const lat = 127.4879630832;
+      const lon = 37.2842684162;
+      const getRestaurantsDto: GetRestaurantsDto = {
+        range: 1,
+        sortBy: 'distance',
+        orderBy: 'ASC',
+        search: 'Sample',
+        pageCount: 10,
+        page: 1,
+      };
+
+      mockRestaurantService.getRestaurants.mockResolvedValue([
+        {
+          id: '1084',
+          placeName: '다도회',
+          latitude: 127.4879630832,
+          longitude: 37.2842684162,
+          business_type: '일식',
+          business_state: 0,
+          road_name_address: '경기도 이천시 부발읍 무촌리 165-5 ',
+          city_name: '이천시',
+          view_count: '0',
+          total_rating: 0,
+          distance: 0.18020829675315922,
+        },
+      ]);
+
+      const result = await mockRestaurantService.getRestaurants(
+        lat,
+        lon,
+        getRestaurantsDto,
+      );
+
+      expect(Array.isArray(result)).toBeTruthy();
+      expect(result.length).toBeGreaterThan(0);
+      result.forEach((restaurant) => {
+        expect(restaurant).toHaveProperty('id');
+        expect(restaurant).toHaveProperty('placeName');
+      });
+    });
+
+    it('위도와 경도값이 비어있음', async () => {
+      const getRestaurantsDto: GetRestaurantsDto = {
+        range: 10,
+        sortBy: 'distance',
+        orderBy: 'ASC',
+        search: 'Sample',
+        pageCount: 10,
+        page: 1,
+      };
+
+      try {
+        await restaurantService.getRestaurants(null, null, getRestaurantsDto);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.message).toBe(FailType.LOCATION_NOT_FOUND);
+      }
+    });
   });
 });
