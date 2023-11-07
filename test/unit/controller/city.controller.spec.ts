83b364aaa1807b167a21447489b63bacf5b24fd3
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-07 23:12:42 +0900

    refactor: 시군구 조회 단위 테스트 수정

--- test/unit/controller/city.controller.spec.ts
+++ test/unit/controller/city.controller.spec.ts
@@ -1,20 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Cache } from 'cache-manager';
 import { CityController } from '../../../src/feature/city/city.controller';
 import { CityService } from '../../../src/feature/city/city.service';
+import { SuccessType } from '../../../src/enum/successType.enum';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { ExternalApiLib } from '../../../src/feature/externalApi/externalApi.lib';
 
 describe('CityController', () => {
-  let controller: CityController;
+  let cityController: CityController;
+  let cityService = {
+    getCities: jest.fn(),
+  };
+  let cacheManager: Cache;
+  let externalApiLib: ExternalApiLib;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CityController],
-      providers: [CityService],
+      providers: [
+        {
+          provide: CityService,
+          useValue: cityService,
+        },
+        {
+          provide: CACHE_MANAGER,
+          useValue: cacheManager,
+        },
+        {
+          provide: ExternalApiLib,
+          useValue: externalApiLib,
+        },
+      ],
     }).compile();
-
-    controller = module.get<CityController>(CityController);
+    cityController = module.get<CityController>(CityController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(cityController).toBeDefined();
+  });
+
+  describe('getCities()', () => {
+    it('시군구 정보 조회 성공', async () => {
+      const sampleCitiesData = ['City1', 'City2', 'City3'];
+      jest.spyOn(cityService, 'getCities').mockResolvedValue(sampleCitiesData);
+
+      const result = await cityController.getCities();
+
+      expect(result).toEqual({
+        message: SuccessType.CITY_FETCH_DISTRICTS,
+        data: sampleCitiesData,
+      });
+    });
   });
 });
