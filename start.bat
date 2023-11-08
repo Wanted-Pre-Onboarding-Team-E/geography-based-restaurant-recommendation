296bb0db6af7651c6b5730607e13743ae4e6aa85
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-08 23:51:32 +0900

    refactor: 스크립트 파일 및 마이그레이션 테이블 수정

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
 
+:: npm 모듈 설치
+START /wait /b cmd /c npm install
+
 :: 데이터베이스 테이블 생성
 START /wait /b cmd /c npm run migration:run
 
-:: Seed 데이터 삽입
-:: START /wait /b cmd /c npm run seed:run
-
-:: NestJS 실행
-npm run start
+:: NestJS 앱 실행
+npm run start:local
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
+# npm 모듈 설치
+npm install
+
 # 데이터베이스 테이블 생성
 npm run migration:run
 
-# Seed 데이터 삽입
-# npm run seed:run
-
-# NestJS 실행
-npm run start
+# NestJS 앱 실행
+npm run start:local
