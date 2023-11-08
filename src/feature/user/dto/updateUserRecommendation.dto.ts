a54ec9d8790e5f191c4f25de068ff6008efa923c
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-11-08 14:20:01 +0900

    refactor: PR 리뷰 반영

--- src/feature/user/dto/updateUserRecommendation.dto.ts
+++ src/feature/user/dto/updateUserRecommendation.dto.ts
@@ -1,7 +1,7 @@
 import { IsBoolean } from 'class-validator';
 import { FailType } from '../../../enum/failType.enum';
 
-export class UpdateUserDto {
+export class UpdateUserRecommendationDto {
   @IsBoolean({ message: FailType.RECOMMENDATION_NOT_BOOLEAN })
   isRecommended!: boolean;
 }
